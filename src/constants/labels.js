ebc5c5f1ff7510e7c59845d245e6a056cd05010d
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-12-07 20:55:36 -0700

    Better tooltip

--- src/constants/labels.js
+++ src/constants/labels.js
@@ -4,20 +4,23 @@ import { common } from './data'
 export const WordCountTooltip = ({ active, payload, label }) => {
   if (payload[0]) {
     const [{ payload: info }] = payload
-    console.log(info)
     if (active) {
       if (info.label === 'Complex') {
         return (
           <div className="custom-tooltip">
             <p className="data">{info.payload.value}</p>
-            <p className="label">The number of unique complex words used</p>
+            <p className="label">
+              The total number of unique complex words used
+            </p>
           </div>
         )
       } else if (info.label === 'Common') {
         return (
           <div className="custom-tooltip">
             <p className="data">{info.payload.value}</p>
-            <p className="label">The number of unique common words used</p>
+            <p className="label">
+              The total number of unique common words used
+            </p>
             <br />
             <p className="intro">Common words include: </p>
             <ul className="common-list">
