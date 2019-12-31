79d7fbdedd1b1f78dcd574d664404ef724eabcd1
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-12-30 17:54:03 -0700

    Implemented antd

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { Card } from 'antd'
 
 const Landing = props => {
-  return <div className="Landing">Search for something</div>
+  return (
+    <div className="Landing">
+      <Card title="the thing is">Here's the thing</Card>
+    </div>
+  )
 }
 export default Landing
